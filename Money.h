dc314fda5073ef55b7f4846074db2a58fdfab58a
Ainsley Nutting <ainsleynutting@Ainsleys-MacBook-Pro.local>
2022-03-26 13:55:11 -0700

    money class 1

--- Money.h
+++ Money.h
@@ -1,1 +1,48 @@
 #include <iostream>
+
+class Money{
+private:
+    int dollars, cents;
+public:
+    Money(){
+        this -> dollars = 0;
+        this -> cents = 0;
+    }
+    Money(int dollars, int cents){
+        this -> dollars = dollars;
+        this -> cents = cents;
+    }
+    friend bool operator == (const Money &lhs, const Money &rhs){
+        return (lhs.dollars==rhs.dollars);
+    }
+    friend bool operator < (const Money &lhs, const Money &rhs){
+        return (lhs.dollars<rhs.dollars);
+    }
+    friend bool operator > (const Money &lhs, const Money &rhs){
+        return (lhs.dollars>rhs.dollars);
+    }
+    friend bool operator >= (const Money &lhs, const Money &rhs){
+        return (lhs.dollars>=rhs.dollars);
+    }
+    friend bool operator <= (const Money &lhs, const Money &rhs){
+        return (lhs.dollars<=rhs.dollars);
+    }
+    friend bool operator != (const Money &lhs, const Money &rhs){
+        return (lhs.dollars!=rhs.dollars);
+    }
+    Money operator+(const Money &all){
+        Money add;
+        add.dollars = this->dollars + all.dollars;
+        add.cents = this->cents + all.cents;
+        return add;
+    }
+    Money operator-(const Money &all){
+        Money sub;
+        sub.dollars = this->dollars - all.dollars;
+        sub.cents = this->cents - all.cents;
+        return sub;
+    }
+    void printMoney(int m){
+        std::cout << m << std::endl;
+    }
+};
