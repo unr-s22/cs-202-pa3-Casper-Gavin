72ed657d5eef90d82c17a7de5b425ccfc92c2879
Gavin Casper <89419475+Casper-Gavin@users.noreply.github.com>
2022-03-30 09:59:14 -0700

    commented changes

--- Account.cpp
+++ Account.cpp
@@ -1,37 +1,36 @@
 #include "Account.h"
 
-Account::Account(const Money& money){
+Account::Account(Money money){
     mMoney = money;
 }
 
-void Account::makeDeposit(const Money& money){
+void Account::makeDeposit(Money money){
     changes.push_back(money);
     changeType.push_back("deposit");
-    update = true;
-    numDeposits++;
+    update = 1;
 }
 
-void Account::makeWithdrawl(const Money& money){
+void Account::makeWithdrawl(Money money){
     changes.push_back(money);
     changeType.push_back("withdrawl");
-    update = true;
-    numWithdrawls++;
+    update = 1;
 }
 
 void Account::updateBalance(){
+    if(update==1){
         for(Money m : changes){
-            int i = 0; 
+            int i = 0;
             if(changeType.at(i)=="withdrawl"){
                 mMoney = mMoney - m;
-                
+                numWithdrawls++;
             } else {
                 mMoney = mMoney + m;
-                
+                numDeposits++;
             }
             i++;
         }
-    
-    changes.clear();
-    changeType.clear();
+    }
+    //changes.clear();
+    //changeType.clear();
     update = 0;
 }
